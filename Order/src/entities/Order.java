680e663c772ac749b06fd09ef5150232742dc345
Pedro Lourenço <pedrolourencojm@gmail.com>
2024-03-20 22:45:11 -0300

    Add files via upload

---
+++ Order/src/entities/Order.java
@@ -0,0 +1,72 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+    private LocalDate moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> items = new ArrayList<>();
+
+    public Order() {
+
+    }
+
+    public Order(LocalDate moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public LocalDate getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDate moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+
+    }
+
+    public Double total() {
+        double sum = 0;
+        for(OrderItem o : items) {
+            sum += o.subTotal();
+        }
+        return sum;
+    }
+
+}
